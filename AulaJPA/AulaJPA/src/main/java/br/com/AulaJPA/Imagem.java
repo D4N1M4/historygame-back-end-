6c90604bf32799cce142db04909a0ec6694c9241
Daniele <danielemaria1407@gmail.com>
2024-12-14 19:09:01 -0300

    Subindo arquivo

---
+++ AulaJPA/AulaJPA/src/main/java/br/com/AulaJPA/Imagem.java
@@ -0,0 +1,58 @@
+package br.com.AulaJPA;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "imagem")
+public class Imagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "lugar_id")
+    private Lugar lugar;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Lugar getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(Lugar lugar) {
+		this.lugar = lugar;
+	}
+    
+    
+}
